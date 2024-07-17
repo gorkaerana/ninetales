bd141a302a90d6806bcc6fb9d2d241a35fbcd51e
Gorka Eraña <eranagorka@gmail.com>
2024-07-17 14:45:54 +0200

    Added exporting functions to 'DataModel'

--- src/ninetales/convert.py
+++ src/ninetales/convert.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
+import collections
 import dataclasses
-from typing import Any, NamedTuple, Type, TYPE_CHECKING
+from typing import Any, ForwardRef, NamedTuple, Type, TYPE_CHECKING
 
 import attrs
 import msgspec
@@ -11,10 +12,10 @@ if TYPE_CHECKING:
 
 class NoDefaultType:
     def __repr__(self):
-        return self.__class__.__name__
+        return type(self).__name__
 
     def __eq__(self, other) -> bool:
-        return isinstance(other, self.__class__)
+        return isinstance(other, type(self))
 
 
 NO_DEFAULT = NoDefaultType()
@@ -26,6 +27,12 @@ MISSING_TYPES = [
 ]
 
 
+"""
+TODO:
+- handle optionally providing type annotations
+"""
+
+
 def no_default_if_misc_missing(o):
     """Returns `NO_DEFAULT` if `o` is one of
     - `attrs.NOTHING`
@@ -35,6 +42,14 @@ def no_default_if_misc_missing(o):
     otherwise it returns `o`
     """
     return NO_DEFAULT if any(o is mt for mt in MISSING_TYPES) else o
+
+
+def resolve_if_forward_ref(t):
+    """ """
+    # TODO: do this only for "base" types
+    if isinstance(t, ForwardRef):
+        return t._evaluate(globals(), locals(), frozenset())
+    return t
 
 
 class AttributeInfo(NamedTuple):
@@ -113,9 +128,39 @@ class DataModel(NamedTuple):
             attributes=[
                 AttributeInfo(
                     name=f,
-                    type=dm_class.__annotations__.get(f),
+                    type=resolve_if_forward_ref(dm_class.__annotations__.get(f)),
                     default=no_default_if_misc_missing(dm_class._field_defaults.get(f)),
                 )
                 for f in dm_class._fields
             ],
         )
+
+    def to_attrs(self):
+        attrs_attributes = {}
+        for a in self.attributes:
+            attrs_kwargs = {"type": a.type}
+            if a.default is not NO_DEFAULT:
+                attrs_kwargs["default"] = a.default
+            attrs_attributes[a.name] = attrs.field(**attrs_kwargs)
+        return attrs.make_class(name=self.name, attrs=attrs_attributes)
+
+    def to_dataclass(self):
+        dataclass_fields = [
+            (a.name, a.type)
+            if a.default is NO_DEFAULT
+            else (a.name, a.type, dataclasses.field(default=a.default))
+            for a in self.attributes
+        ]
+        return dataclasses.make_dataclass(self.name, dataclass_fields)
+
+    def to_msgspec(self):
+        msgspec_fields = [
+            (a.name, a.type)
+            if a.default is NO_DEFAULT
+            else (a.name, a.type, msgspec.field(default=a.default))
+            for a in self.attributes
+        ]
+        return msgspec.defstruct(self.name, msgspec_fields)
+
+    def to_namedtuple(self):
+        return collections.namedtuple(self.name, [a.name for a in self.attributes])
